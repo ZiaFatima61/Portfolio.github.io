4a30ca818094dff6fcf06205e44ba429810c86ce
ZiaFatima61 <01fe24bcs305@kletech.ac.in>
2025-12-22 22:23:55 +0530

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,10 @@
 function togglePage(pageId) {
   const pages = ['shoppingMall', 'forestFire', 'tourism', 'waterElectricity'];
+
   pages.forEach(id => {
-    document.getElementById(id).style.display = (id === pageId) ? 'block' : 'none';
+    document.getElementById(id).style.display =
+      (id === pageId) ? 'block' : 'none';
   });
+
+  window.scrollTo({ top: 0, behavior: 'instant' });
 }
